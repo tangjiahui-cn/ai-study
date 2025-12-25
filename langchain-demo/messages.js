2d647cc72ba10108e3e8fdc2a0e55de9c43d8caa
tangjiahui <tjhaaa@163.com>
2025-12-25 10:25:10 +0800

    int

---
+++ langchain-demo/messages.js
@@ -0,0 +1,10 @@
+import { HumanMessage, AIMessage, SystemMessage } from "langchain";
+
+const messages = [
+    new SystemMessage("你是一个大屏技术专家"),
+    new HumanMessage("你好"),
+    new AIMessage("我不好"),
+    new HumanMessage("你为什么不好？"),
+];
+
+console.log("zz -> ", messages);
